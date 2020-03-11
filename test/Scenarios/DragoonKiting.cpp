f1f71d057bc97dc32772c4c18dd23ea84552416b
Bruce Nielsen <bruce.nielsen@maersk.com>
2020-03-11 13:36:49 +0100

    First iteration of boid-based dragoon kiting

---
+++ test/Scenarios/DragoonKiting.cpp
@@ -0,0 +1,94 @@
+#include "BWTest.h"
+#include "DoNothingModule.h"
+
+#include "Map.h"
+#include "Strategist.h"
+#include "TestAttackBasePlay.h"
+
+namespace
+{
+    class AttackNearestUnitModule : public DoNothingModule
+    {
+    public:
+        void onFrame() override
+        {
+            for (auto unit : BWAPI::Broodwar->self()->getUnits())
+            {
+                if (!unit->canAttack() || unit->getType().isWorker()) continue;
+
+                int closestDist = INT_MAX;
+                BWAPI::Unit closest = nullptr;
+                for (auto enemy : BWAPI::Broodwar->enemy()->getUnits())
+                {
+                    if (!enemy->exists() || !enemy->isVisible()) continue;
+
+                    auto dist = unit->getDistance(enemy);
+                    if (dist < closestDist)
+                    {
+                        closestDist = dist;
+                        closest = enemy;
+                    }
+                }
+
+                if (closest)
+                {
+                    BWAPI::UnitCommand currentCommand(unit->getLastCommand());
+                    if (currentCommand.getType() == BWAPI::UnitCommandTypes::Attack_Unit &&
+                        currentCommand.getTarget() == closest)
+                    {
+                        continue;
+                    }
+
+                    unit->attack(closest);
+                }
+            }
+        }
+    };
+}
+
+// One dragoon vs. one zergling: assert the zergling doesn't get as many hits as it otherwise would
+TEST(DragoonKiting, DragoonVsZergling)
+{
+    BWTest test;
+    test.opponentRace = BWAPI::Races::Zerg;
+    test.opponentModule = []()
+    {
+        return new AttackNearestUnitModule();
+    };
+    test.map = "maps/sscai/(4)Fighting Spirit.scx";
+    test.frameLimit = 500;
+    test.expectWin = false;
+
+    test.myInitialUnits = {
+            UnitTypeAndPosition(BWAPI::UnitTypes::Protoss_Dragoon, BWAPI::TilePosition(116, 99)),
+    };
+
+    test.opponentInitialUnits = {
+            UnitTypeAndPosition(BWAPI::UnitTypes::Zerg_Zergling, BWAPI::TilePosition(116, 106)),
+    };
+
+    Base *baseToAttack = nullptr;
+
+    // Order the dragoon to attack the bottom base
+    test.onStartMine = [&baseToAttack]()
+    {
+        baseToAttack = Map::baseNear(BWAPI::Position(BWAPI::TilePosition(117, 117)));
+
+        std::vector<std::shared_ptr<Play>> openingPlays;
+        openingPlays.emplace_back(std::make_shared<TestAttackBasePlay>(baseToAttack));
+        Strategist::setOpening(openingPlays);
+    };
+
+    test.onEndMine = [](bool win)
+    {
+        for (auto &unit : BWAPI::Broodwar->self()->getUnits())
+        {
+            if (unit->getType() != BWAPI::UnitTypes::Protoss_Dragoon) continue;
+
+            // Shields will be approx. 30 if we don't kite
+            EXPECT_GT(unit->getShields(), 70);
+        }
+    };
+
+    test.run();
+}
