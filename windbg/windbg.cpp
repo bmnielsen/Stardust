838e946668b2f1730705b732217d269448bee9fb
Bruce Nielsen <bruce.nielsen@maersk.com>
2020-05-11 19:27:27 +0200

    Small utility for debugging on Windows

---
+++ windbg/windbg.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include "windows.h"
+
+namespace
+{
+    void loadModule(const std::string &path)
+    {
+        std::cout << "Loading module from " << path << "..." << std::endl;
+
+        HMODULE hMod = LoadLibraryA(path.c_str());
+
+        std::cout << "Module loaded! Address: " << std::hex << (unsigned long) hMod << std::endl;
+    }
+}
+
+int main()
+{
+    loadModule("C:\\Dev\\BW\\Stardust-dev\\out\\src\\RelWithDebInfo\\Stardust.dll");
+
+    while (true)
+    {
+    }
+}
