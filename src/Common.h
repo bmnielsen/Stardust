505ae890875f0c1fba14ee89261b8801bc830579
Bruce Nielsen <bruce.nielsen@maersk.com>
2020-05-12 08:41:20 +0200

    Moving some more debug things to verbose logging

--- src/Common.h
+++ src/Common.h
@@ -8,7 +8,7 @@
 // Defines that control instrumentation across the whole project
 // Instrumentation of specific features is enabled in the relevant files
 // The overall CHERRYVIS_ENABLED define is in CherryVis.h
-#if INSTRUMENTATION_ENABLED
+#if INSTRUMENTATION_ENABLED_VERBOSE
 #define DEBUG_GRID_UPDATES false // Writes a log message whenever a grid is updated
 #define DEBUG_COMBATSIM true    // Writes log messages for each sim result
 #define DEBUG_UNIT_ORDERS true   // Writes a log message for each order sent to a unit
