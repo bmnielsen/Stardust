1ddf56feb38d1f14541ddefdf1b3faf153fd91cc
Bruce Nielsen <bruce.nielsen@maersk.com>
2020-05-31 14:10:44 +0200

    Fix up issues with units not attacking while holding a choke

--- test/Scenarios/ContainAtChoke.cpp
+++ test/Scenarios/ContainAtChoke.cpp
@@ -6,6 +6,8 @@
 #include "TestAttackBasePlay.h"
 #include "Units.h"
 
+#include "Plays/MainArmy/DefendMyMain.h"
+
 namespace
 {
     bool isCombatUnit(BWAPI::UnitType type)
@@ -152,6 +154,89 @@ TEST(ContainAtChoke, ZealotsAndDragoonsVsZerglingsOnBridge)
 
         std::vector<std::shared_ptr<Play>> openingPlays;
         openingPlays.emplace_back(std::make_shared<TestAttackBasePlay>(baseToAttack));
+        Strategist::setOpening(openingPlays);
+    };
+
+    test.onEndMine = [](bool)
+    {
+        // Ensure there are no known enemy combat units
+        bool hasEnemyCombatUnit = false;
+        for (auto &unit : Units::allEnemy())
+        {
+            if (isCombatUnit(unit->type))
+            {
+                hasEnemyCombatUnit = true;
+                break;
+            }
+        }
+
+        EXPECT_FALSE(hasEnemyCombatUnit);
+    };
+
+    test.run();
+}
+
+// 25 lings vs. 8 zealots and 2 goons
+TEST(ContainAtChoke, ZealotsAndDragoonsVsZerglingsOnRamp)
+{
+    BWTest test;
+    test.opponentRace = BWAPI::Races::Protoss;
+    test.opponentModule = []()
+    {
+        return new AttackAtFrameModule(BWAPI::Position(BWAPI::WalkPosition(50, 375)), 200);
+    };
+    test.map = Maps::GetOne("Andromeda");
+    test.randomSeed = 19968;
+    test.frameLimit = 2000;
+    test.expectWin = false;
+
+    // Equal dragoon armies at each side of the choke
+    test.myInitialUnits = {
+            UnitTypeAndPosition(BWAPI::UnitTypes::Protoss_Zealot, BWAPI::TilePosition(14, 95)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Protoss_Zealot, BWAPI::TilePosition(15, 95)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Protoss_Zealot, BWAPI::TilePosition(16, 95)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Protoss_Zealot, BWAPI::TilePosition(17, 95)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Protoss_Zealot, BWAPI::TilePosition(14, 96)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Protoss_Zealot, BWAPI::TilePosition(15, 96)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Protoss_Zealot, BWAPI::TilePosition(16, 96)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Protoss_Zealot, BWAPI::TilePosition(17, 96)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Protoss_Dragoon, BWAPI::TilePosition(12, 93)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Protoss_Dragoon, BWAPI::TilePosition(12, 94)),
+    };
+
+    test.opponentInitialUnits = {
+            UnitTypeAndPosition(BWAPI::UnitTypes::Zerg_Zergling, BWAPI::WalkPosition(127, 375)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Zerg_Zergling, BWAPI::WalkPosition(128, 375)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Zerg_Zergling, BWAPI::WalkPosition(129, 375)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Zerg_Zergling, BWAPI::WalkPosition(130, 375)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Zerg_Zergling, BWAPI::WalkPosition(131, 375)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Zerg_Zergling, BWAPI::WalkPosition(127, 376)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Zerg_Zergling, BWAPI::WalkPosition(128, 376)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Zerg_Zergling, BWAPI::WalkPosition(129, 376)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Zerg_Zergling, BWAPI::WalkPosition(130, 376)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Zerg_Zergling, BWAPI::WalkPosition(131, 376)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Zerg_Zergling, BWAPI::WalkPosition(127, 377)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Zerg_Zergling, BWAPI::WalkPosition(128, 377)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Zerg_Zergling, BWAPI::WalkPosition(129, 377)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Zerg_Zergling, BWAPI::WalkPosition(130, 377)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Zerg_Zergling, BWAPI::WalkPosition(131, 377)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Zerg_Zergling, BWAPI::WalkPosition(127, 378)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Zerg_Zergling, BWAPI::WalkPosition(128, 378)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Zerg_Zergling, BWAPI::WalkPosition(129, 378)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Zerg_Zergling, BWAPI::WalkPosition(130, 378)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Zerg_Zergling, BWAPI::WalkPosition(131, 378)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Zerg_Zergling, BWAPI::WalkPosition(127, 379)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Zerg_Zergling, BWAPI::WalkPosition(128, 379)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Zerg_Zergling, BWAPI::WalkPosition(129, 379)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Zerg_Zergling, BWAPI::WalkPosition(130, 379)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Zerg_Zergling, BWAPI::WalkPosition(131, 379)),
+    };
+
+    // Order the dragoon to attack the bottom base
+    test.onStartMine = []()
+    {
+        std::vector<std::shared_ptr<Play>> openingPlays;
+        openingPlays.emplace_back(std::make_shared<DefendMyMain>());
         Strategist::setOpening(openingPlays);
     };
 
