a4deb1cacec9a1480af51d609e4cb610a417ef8b
Bruce Nielsen <bruce.nielsen@maersk.com>
2020-07-23 10:56:50 +0200

    Don't add defend base plays for main / natural if the main army is close to them

--- src/Strategist/StrategyEngines/Common/StrategyEngine.cpp
+++ src/Strategist/StrategyEngines/Common/StrategyEngine.cpp
@@ -1,4 +1,5 @@
 #include <Strategist/Plays/MainArmy/DefendMyMain.h>
+#include <Strategist/Plays/MainArmy/AttackEnemyMain.h>
 #include <Strategist/Plays/Defensive/DefendBase.h>
 #include "StrategyEngine.h"
 
@@ -6,22 +7,38 @@
 
 void StrategyEngine::UpdateDefendBasePlays(std::vector<std::shared_ptr<Play>> &plays)
 {
+    auto mainArmyPlay = getPlay<MainArmyPlay>(plays);
+
     // First gather the list of bases we want to defend
     std::set<Base *> basesToDefend;
-    for (auto &base : Map::getMyBases())
-    {
-        if (base == Map::getMyMain())
-        {
-            // Don't defend it with a DefendBase play if our main army is already defending the base
-            auto mainArmyPlay = getPlay<MainArmyPlay>(plays);
-            if (mainArmyPlay && typeid(*mainArmyPlay) == typeid(DefendMyMain)) continue;
-        }
-        else if (base->mineralPatchCount() < 3)
-        {
-            continue;
-        }
 
-        basesToDefend.insert(base);
+    // Don't defend any bases if our main army play is defending our main
+    if (mainArmyPlay && typeid(*mainArmyPlay) == typeid(DefendMyMain))
+    {
+        for (auto &base : Map::getMyBases())
+        {
+            if (base == Map::getMyMain() || base == Map::getMyNatural())
+            {
+                // Don't defend our main or natural with a DefendBase play if our main army is close to it
+                if (mainArmyPlay && typeid(*mainArmyPlay) == typeid(AttackEnemyMain))
+                {
+                    auto vanguard = mainArmyPlay->getSquad()->vanguardCluster();
+                    if (vanguard && vanguard->vanguard)
+                    {
+                        int vanguardDist = PathFinding::GetGroundDistance(vanguard->vanguard->lastPosition,
+                                                                          base->getPosition(),
+                                                                          BWAPI::UnitTypes::Protoss_Zealot);
+                        if (vanguardDist != -1 && vanguardDist < 1500) continue;
+                    }
+                }
+            }
+            else if (base->mineralPatchCount() < 3)
+            {
+                continue;
+            }
+
+            basesToDefend.insert(base);
+        }
     }
 
     // Scan the plays and remove those that are no longer relevant
