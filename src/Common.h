516bc741fa68a616061c0387c5c14f40e4812d48
Bruce Nielsen <bruce.nielsen@maersk.com>
2020-07-29 09:17:16 +0200

    Debug unit orders in non-verbose mode

--- src/Common.h
+++ src/Common.h
@@ -8,8 +8,12 @@
 // Defines that control instrumentation across the whole project
 // Instrumentation of specific features is enabled in the relevant files
 // The overall CHERRYVIS_ENABLED define is in CherryVis.h
+
+#if INSTRUMENTATION_ENABLED
+#define DEBUG_UNIT_ORDERS true   // Writes a log message for each order sent to a unit
+#endif
+
 #if INSTRUMENTATION_ENABLED_VERBOSE
 #define DEBUG_GRID_UPDATES false // Writes a log message whenever a grid is updated
 #define DEBUG_COMBATSIM true    // Writes log messages for each sim result
-#define DEBUG_UNIT_ORDERS true   // Writes a log message for each order sent to a unit
 #endif
