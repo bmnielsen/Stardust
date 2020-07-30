48895a14d79522ff20b8022f7c501732a1095a4e
Bruce Nielsen <bmnielsen@users.noreply.github.com>
2020-07-30 15:25:31 +0200

    Added batch file to remove everything non-essential for tournament builds

---
+++ minify.cmd
@@ -0,0 +1,17 @@
+@echo off
+
+rmdir .git /s /q
+rmdir 3rdparty\nlohmann /s /q
+rmdir 3rdparty\openbw /s /q
+rmdir 3rdparty\zstd /s /q
+rmdir 3rdparty\zstdstream /s /q
+rmdir docs /s /q
+rmdir test /s /q
+rmdir windbg /s /q
+del .gitignore
+del CMakeLists.txt
+del 3rdparty\BWAPILIB\CMakeLists.txt
+del 3rdparty\BWEM\CMakeLists.txt
+del 3rdparty\FAP\CMakeLists.txt
+del src\CMakeLists.txt
+del minify.cmd
