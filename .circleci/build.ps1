be5a102a5b62a223c4887f8003a6febcfdb36cf7
Bruce Nielsen <bruce.nielsen@maersk.com>
2020-10-21 12:59:58 +0200

    Add artifact storing and caching

--- .circleci/build.ps1
+++ .circleci/build.ps1
@@ -4,7 +4,10 @@ if (-not (Get-Command cmake -ErrorAction SilentlyContinue)) {
     New-Alias -Name cmake -Value "$Env:ProgramFiles\CMake\bin\cmake.exe"
 }
 
-New-Item -Name build -ItemType Directory
+if (! (Test-Path -Path build)) {
+    New-Item -Name build -ItemType Directory
+}
+
 Push-Location build
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_GENERATOR_PLATFORM=Win32 ..
 cmake --build . --config RelWithDebInfo
