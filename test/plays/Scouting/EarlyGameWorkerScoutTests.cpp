79fdcae0275de3624299020004569428a0b13af1
Bruce Nielsen <bruce.nielsen@maersk.com>
2021-01-26 09:58:34 +0100

    Improve scout survivability

---
+++ test/plays/Scouting/EarlyGameWorkerScoutTests.cpp
@@ -0,0 +1,31 @@
+#include "BWTest.h"
+#include "BananaBrain.h"
+
+TEST(EarlyGameWorkerScoutTests, MonitorsEnemyChoke)
+{
+    BWTest test;
+    BananaBrain *bbModule;
+    test.opponentRace = BWAPI::Races::Zerg;
+    test.map = Maps::GetOne("Glaive");
+    test.randomSeed = 42;
+    test.frameLimit = 6000;
+    test.expectWin = false;
+    test.opponentModule = [&]()
+    {
+        bbModule = new BananaBrain();
+        bbModule->strategyName = ZergStrategy::kZvP_2HatchLing;
+        return bbModule;
+    };
+    test.onStartOpponent = []()
+    {
+        std::cout.setstate(std::ios_base::failbit);
+    };
+    test.onEndMine = [](bool won)
+    {
+        // Verify the scout is still doing something
+//        EXPECT_NE(Strategist::getWorkerScoutStatus(), Strategist::WorkerScoutStatus::ScoutingFailed);
+//        EXPECT_NE(Strategist::getWorkerScoutStatus(), Strategist::WorkerScoutStatus::ScoutingCompleted);
+//        EXPECT_NE(Strategist::getWorkerScoutStatus(), Strategist::WorkerScoutStatus::ScoutingBlocked);
+    };
+    test.run();
+}
