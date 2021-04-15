2cf628213444dbd8c4e8ef59e9764338d9b73caa
Bruce Nielsen <bruce.nielsen@maersk.com>
2021-04-15 13:26:57 +0200

    Handle case where our units get stuck on terrain while trying to contain tanks

--- test/Scenarios/ContainTanks.cpp
+++ test/Scenarios/ContainTanks.cpp
@@ -46,3 +46,46 @@ TEST(ContainTanks, FSHighGround)
 
     test.run();
 }
+
+TEST(ContainTanks, FSNatural)
+{
+    BWTest test;
+    test.opponentRace = BWAPI::Races::Terran;
+    test.opponentModule = []()
+    {
+        return new DoNothingModule();
+    };
+    test.map = Maps::GetOne("Fighting");
+    test.randomSeed = 42;
+    test.frameLimit = 800;
+    test.expectWin = false;
+
+    // Equal dragoon armies at each side of the choke
+    test.myInitialUnits = {
+            UnitTypeAndPosition(BWAPI::UnitTypes::Protoss_Dragoon, BWAPI::TilePosition(111, 85)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Protoss_Dragoon, BWAPI::TilePosition(112, 85)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Protoss_Probe, BWAPI::TilePosition(110, 96)), // For vision on enemy army
+    };
+
+    test.opponentInitialUnits = {
+            UnitTypeAndPosition(BWAPI::UnitTypes::Terran_Siege_Tank_Siege_Mode, BWAPI::TilePosition(110, 95)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Terran_Vulture, BWAPI::TilePosition(111, 95)),
+            UnitTypeAndPosition(BWAPI::UnitTypes::Terran_Vulture, BWAPI::TilePosition(110, 94)),
+    };
+
+    Base *baseToAttack = nullptr;
+
+    // Order the dragoons to attack the bottom base
+    test.onStartMine = [&baseToAttack]()
+    {
+        baseToAttack = Map::baseNear(BWAPI::Position(BWAPI::TilePosition(118, 118)));
+
+        Strategist::setStrategyEngine(std::make_unique<DoNothingStrategyEngine>());
+
+        std::vector<std::shared_ptr<Play>> openingPlays;
+        openingPlays.emplace_back(std::make_shared<TestMainArmyAttackBasePlay>(baseToAttack));
+        Strategist::setOpening(openingPlays);
+    };
+
+    test.run();
+}
