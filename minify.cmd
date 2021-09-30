dcc167bf9f4aaa6ddbe92afceeb1ee34d65a0e61
Bruce Nielsen <bruce.nielsen@maersk.com>
2021-09-30 09:41:27 +0200

    Fix minify

--- minify.cmd
+++ minify.cmd
@@ -1,13 +1,13 @@
 @echo off
 
 rmdir .git /s /q
-rmdir 3rdparty\nlohmann /s /q
 rmdir 3rdparty\openbw /s /q
 rmdir 3rdparty\zstd /s /q
 rmdir 3rdparty\zstdstream /s /q
 rmdir docs /s /q
 rmdir test /s /q
 rmdir windbg /s /q
+rmdir out /s /q
 del .gitignore
 del CMakeLists.txt
 del 3rdparty\BWAPILIB\CMakeLists.txt
