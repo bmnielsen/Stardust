5e8024482ef12b668a67218419cb55499800330c
Bruce Nielsen <bmnielsen@users.noreply.github.com>
2022-02-21 13:27:26 +0100

    Make an anti-sunken-contain strategy to overwhelm the contain with upgraded zealots

--- test/Scenarios/SunkenContain.cpp
+++ test/Scenarios/SunkenContain.cpp
@@ -5,8 +5,8 @@ TEST(SunkenContain, Steamhammer)
 {
     BWTest test;
     test.opponentName = "Steamhammer";
-    test.map = Maps::GetOne("Spirit");
-    test.randomSeed = 50443;
+    test.map = Maps::GetOne("Mancha");
+    test.randomSeed = 45965;
     test.frameLimit = 15000;
     test.opponentRace = BWAPI::Races::Zerg;
     test.opponentModule = []()
@@ -23,53 +23,29 @@ TEST(SunkenContain, Steamhammer)
             UnitTypeAndPosition unit(type, BWAPI::Position(pos));
             BWAPI::Broodwar->createUnit(BWAPI::Broodwar->enemy(), unit.type, unit.getCenterPosition());
         };
-        if (BWAPI::Broodwar->getFrameCount() == 2000)
+        if (BWAPI::Broodwar->getFrameCount() == 6000)
         {
-            create(BWAPI::UnitTypes::Zerg_Overlord, BWAPI::TilePosition(110, 88));
+            create(BWAPI::UnitTypes::Zerg_Overlord, BWAPI::TilePosition(92, 8));
         }
-        if (BWAPI::Broodwar->getFrameCount() == 2010)
+        if (BWAPI::Broodwar->getFrameCount() == 6000)
         {
-            create(BWAPI::UnitTypes::Zerg_Hatchery, BWAPI::TilePosition(110, 88));
+            create(BWAPI::UnitTypes::Zerg_Hatchery, BWAPI::TilePosition(92, 8));
         }
-        if (BWAPI::Broodwar->getFrameCount() == 2900)
+        if (BWAPI::Broodwar->getFrameCount() == 7000)
         {
-            create(BWAPI::UnitTypes::Zerg_Sunken_Colony, BWAPI::TilePosition(107, 91));
+            create(BWAPI::UnitTypes::Zerg_Sunken_Colony, BWAPI::TilePosition(96, 9));
         }
-        if (BWAPI::Broodwar->getFrameCount() == 2910)
+        if (BWAPI::Broodwar->getFrameCount() == 7010)
         {
-            create(BWAPI::UnitTypes::Zerg_Sunken_Colony, BWAPI::TilePosition(109, 91));
+            create(BWAPI::UnitTypes::Zerg_Sunken_Colony, BWAPI::TilePosition(95, 11));
         }
-        if (BWAPI::Broodwar->getFrameCount() == 2920)
+        if (BWAPI::Broodwar->getFrameCount() == 7020)
         {
-            create(BWAPI::UnitTypes::Zerg_Sunken_Colony, BWAPI::TilePosition(111, 91));
+            create(BWAPI::UnitTypes::Zerg_Sunken_Colony, BWAPI::TilePosition(98, 7));
         }
         if (BWAPI::Broodwar->getFrameCount() == 2930)
         {
-            create(BWAPI::UnitTypes::Zerg_Sunken_Colony, BWAPI::TilePosition(113, 91));
-        }
-        if (BWAPI::Broodwar->getFrameCount() == 2940)
-        {
-            create(BWAPI::UnitTypes::Zerg_Sunken_Colony, BWAPI::TilePosition(107, 93));
-        }
-        if (BWAPI::Broodwar->getFrameCount() == 2950)
-        {
-            create(BWAPI::UnitTypes::Zerg_Sunken_Colony, BWAPI::TilePosition(109, 93));
-        }
-        if (BWAPI::Broodwar->getFrameCount() == 2960)
-        {
-            create(BWAPI::UnitTypes::Zerg_Sunken_Colony, BWAPI::TilePosition(111, 93));
-        }
-        if (BWAPI::Broodwar->getFrameCount() == 2970)
-        {
-            create(BWAPI::UnitTypes::Zerg_Sunken_Colony, BWAPI::TilePosition(113, 93));
-        }
-        if (BWAPI::Broodwar->getFrameCount() == 2980)
-        {
-            create(BWAPI::UnitTypes::Zerg_Sunken_Colony, BWAPI::TilePosition(106, 89));
-        }
-        if (BWAPI::Broodwar->getFrameCount() == 2990)
-        {
-            create(BWAPI::UnitTypes::Zerg_Sunken_Colony, BWAPI::TilePosition(112, 89));
+            create(BWAPI::UnitTypes::Zerg_Sunken_Colony, BWAPI::TilePosition(99, 5));
         }
     };
 
