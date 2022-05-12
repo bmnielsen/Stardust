ff684337ac6ff31db993ba53f2fa0bd2a3750d74
Bruce Nielsen <bmnielsen@users.noreply.github.com>
2022-05-12 23:40:08 +0200

    More boids WIP

--- test/plays/Scouting/EarlyGameWorkerScoutTests.cpp
+++ test/plays/Scouting/EarlyGameWorkerScoutTests.cpp
@@ -29,3 +29,32 @@ TEST(EarlyGameWorkerScoutTests, MonitorsEnemyChoke)
     };
     test.run();
 }
+
+TEST(EarlyGameWorkerScoutTests, SurvivabilityVsProtossRush)
+{
+    BWTest test;
+    BananaBrain *bbModule;
+    test.opponentRace = BWAPI::Races::Protoss;
+    test.map = Maps::GetOne("Glaive");
+    test.randomSeed = 42;
+    test.frameLimit = 5000;
+    test.expectWin = false;
+    test.opponentModule = [&]()
+    {
+        bbModule = new BananaBrain();
+        bbModule->strategyName = ProtossStrategy::kPvP_99Gate;
+        return bbModule;
+    };
+    test.onStartOpponent = []()
+    {
+        std::cout.setstate(std::ios_base::failbit);
+    };
+    test.onEndMine = [](bool won)
+    {
+        // Verify the scout is still doing something
+//        EXPECT_NE(Strategist::getWorkerScoutStatus(), Strategist::WorkerScoutStatus::ScoutingFailed);
+//        EXPECT_NE(Strategist::getWorkerScoutStatus(), Strategist::WorkerScoutStatus::ScoutingCompleted);
+//        EXPECT_NE(Strategist::getWorkerScoutStatus(), Strategist::WorkerScoutStatus::ScoutingBlocked);
+    };
+    test.run();
+}
