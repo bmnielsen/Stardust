2c62c60a893d7eec1f471991d4dd53d183fd54f9
Bruce Nielsen <bmnielsen@users.noreply.github.com>
2022-06-21 21:44:50 +0200

    McRave test thirty

--- test/FullGame/McRave.cpp
+++ test/FullGame/McRave.cpp
@@ -4,6 +4,64 @@
 #include "Header.h"
 #include "3rdparty/opponents/McRave/McRave/BuildOrder.h"
 
+TEST(McRave, RunThirty)
+{
+    int count = 0;
+    int lost = 0;
+    while (count < 30)
+    {
+        BWTest test;
+        McRaveModule* mcraveModule;
+        test.opponentName = "McRave";
+        test.opponentRace = BWAPI::Races::Zerg;
+        test.maps = Maps::Get("sscait");
+        test.opponentModule = [&]()
+        {
+            mcraveModule = new McRaveModule();
+            return mcraveModule;
+        };
+        test.onStartOpponent = [&]()
+        {
+            std::ostringstream os;
+            os << McRave::BuildOrder::getCurrentBuild()
+               << "_" << McRave::BuildOrder::getCurrentOpener()
+               << "_" << McRave::BuildOrder::getCurrentTransition();
+
+            auto build = os.str();
+
+            std::cout << "McRave strategy: " << build << std::endl;
+            if (test.sharedMemory)
+            {
+                strncpy(test.sharedMemory,
+                        build.c_str(),
+                        std::min(255UL, build.size()));
+            }
+
+            std::cout.setstate(std::ios_base::failbit);
+        };
+        test.onEndMine = [&](bool won)
+        {
+            std::ostringstream replayName;
+            replayName << "McRave_" << test.map->shortname();
+            if (!won)
+            {
+                replayName << "_LOSS";
+            }
+            if (test.sharedMemory) replayName << "_" << test.sharedMemory;
+            replayName << "_" << test.randomSeed;
+            test.replayName = replayName.str();
+
+            count++;
+            std::cout << "---------------------------------------------" << std::endl;
+            std::cout << "STATUS AFTER " << count << " GAME" << (count == 1 ? "" : "S") << ": "
+                      << (count - lost) << " won; " << lost << " lost" << std::endl;
+            std::cout << "---------------------------------------------" << std::endl;
+        };
+        test.expectWin = false;
+        test.run();
+    }
+}
+
 TEST(McRave, RunOne)
 {
     BWTest test;
@@ -56,7 +114,8 @@ TEST(McRave, PoolHatch_9Pool_2HatchMuta)
     McRaveModule* mcraveModule;
     test.opponentName = "McRave";
     test.opponentRace = BWAPI::Races::Zerg;
-    test.maps = Maps::Get("sscait");
+    test.map = Maps::GetOne("Heartbreak");
+    test.randomSeed = 20126;
     test.opponentModule = [&]()
     {
         mcraveModule = new McRaveModule();
