34485faaaddbf91f46859d07b54614d4d6d4f0be
Bruce Nielsen <bmnielsen@users.noreply.github.com>
2022-07-18 07:37:40 +0200

    Cancel hidden base play if it is scouted

--- src/Strategist/Plays/Macro/HiddenBase.cpp
+++ src/Strategist/Plays/Macro/HiddenBase.cpp
@@ -3,6 +3,7 @@
 #include "Map.h"
 #include "Workers.h"
 #include "Builder.h"
+#include "Units.h"
 
 void HiddenBase::update()
 {
@@ -13,28 +14,34 @@ void HiddenBase::update()
         if (!base) return;
     }
 
-    // If our builder died, cancel the play
-    // TODO: Is this too extreme?
+    // Stop the play when:
+    // - We are past frame 15000
+    // - The builder died
+    // - We have taken our natural
+    // - The enemy has scouted the base
+    if (currentFrame > 15000)
+    {
+        status.complete = true;
+        return;
+    }
+
     if (builder && !builder->exists())
     {
         status.complete = true;
         return;
     }
 
-    // Stop the play when:
-    // - We are past frame 15000
-    // - We have taken our natural
     auto natural = Map::getMyNatural();
-    if (currentFrame > 15000 ||
-        (natural && natural->owner == BWAPI::Broodwar->self() && natural->resourceDepot && natural->resourceDepot->completed))
+    if (natural && natural->owner == BWAPI::Broodwar->self() && natural->resourceDepot && natural->resourceDepot->completed)
     {
-        if (builder)
-        {
-            CherryVis::log(builder->id) << "Releasing from hidden base play";
-            Builder::releaseReservedBuilder(builder);
-            Workers::releaseWorker(builder);
-        }
+        status.complete = true;
+        return;
+    }
 
+    std::set<Unit> enemyUnits;
+    Units::enemyInRadius(enemyUnits, base->getPosition(), 640);
+    if (!enemyUnits.empty())
+    {
         status.complete = true;
         return;
     }
@@ -62,4 +69,19 @@ void HiddenBase::update()
     {
         builder->moveTo(base->getPosition());
     }
+}
+
+void HiddenBase::disband(const std::function<void(const MyUnit)> &removedUnitCallback,
+                         const std::function<void(const MyUnit)> &movableUnitCallback)
+{
+    Builder::cancelBase(base);
+
+    if (builder)
+    {
+        CherryVis::log(builder->id) << "Releasing from hidden base play";
+        Builder::releaseReservedBuilder(builder);
+        Workers::releaseWorker(builder);
+    }
+
+    Play::disband(removedUnitCallback, movableUnitCallback);
 }
