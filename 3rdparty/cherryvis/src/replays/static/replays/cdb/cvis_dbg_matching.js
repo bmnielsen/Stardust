8dea93c20163c818e2ab0146c4608514c1bb672c
Bruce Nielsen <bmnielsen@users.noreply.github.com>
2022-08-09 14:28:44 +0200

    Skip unit matching if no cvis data is available

--- 3rdparty/cherryvis/src/replays/static/replays/cdb/cvis_dbg_matching.js
+++ 3rdparty/cherryvis/src/replays/static/replays/cdb/cvis_dbg_matching.js
@@ -32,16 +32,20 @@ function cvis_dbg_match_units(global_data, cvis_state) {
     cvis_state.units_first_seen_frame = {};
     var matcher = Module.get_units_matcher();
     var all_units_to_match = get_all_units_to_match();
-    $.each(all_units_to_match, function(_, first_seen_infos) {
-      cvis_state.units_first_seen_frame[first_seen_infos.id] = first_seen_infos.frame;
-      matcher.add_unit(
-        parseInt(first_seen_infos.frame), first_seen_infos.id,
-        first_seen_infos.type, first_seen_infos.x,
-        first_seen_infos.y
-      );
-    });
-    pbar.width('0%');
-    return null;
+    if (all_units_to_match.length === 0) {
+      cvis_state.units_matching.skipped = true;
+    } else {
+      $.each(all_units_to_match, function(_, first_seen_infos) {
+        cvis_state.units_first_seen_frame[first_seen_infos.id] = first_seen_infos.frame;
+        matcher.add_unit(
+          parseInt(first_seen_infos.frame), first_seen_infos.id,
+          first_seen_infos.type, first_seen_infos.x,
+          first_seen_infos.y
+        );
+      });
+      pbar.width('0%');
+      return null;
+    }
   }
   if (cvis_state.units_matching.skipped) {
     cvis_state.id2bw = {};
