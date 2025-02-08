151bd07dbfa3b6ebf31785b27646e546a294b955
Bruce Nielsen <bmnielsen@users.noreply.github.com>
2025-02-08 14:37:45 +0100

    Treat spawn location as a path starting at the depot

--- src/Workers/MiningOptimization/WorkerGatherStatus.cpp
+++ src/Workers/MiningOptimization/WorkerGatherStatus.cpp
@@ -12,7 +12,12 @@ namespace WorkerMiningOptimization
         if (positionHistory.empty())
         {
             // For the first position, compute whether the path started at the depot
-            pathStartsAtDepot = ((depot->getDistance(worker) == 0) && (resource->getDistance(depot) < 256));
+            // The logic for this is to treat it as starting at the depot if the distance to the depot was 0 or
+            // the start position was the unit's spawn location (which is not quite at distance 0)
+            // We exclude gathers from depots that are a long way away though to avoid tracking distance mining paths
+            pathStartsAtDepot = (
+                    (resource->getDistance(depot) < 256) && (
+                            (depot->getDistance(worker) == 0) || (worker->lastPosition == worker->spawnPosition)));
             currentPosition = std::make_shared<PositionAndVelocity>(worker, nullptr);
         }
         else
