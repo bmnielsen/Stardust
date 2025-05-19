cc73d0dc0873491e435055d364165750310ab750
Bruce Nielsen <bmnielsen@users.noreply.github.com>
2025-05-19 21:44:31 +0200

    Make mining data shrinker work for a map pool

--- test/Misc/MiningDataShrinker.cpp
+++ test/Misc/MiningDataShrinker.cpp
@@ -1,35 +1,62 @@
 #include "gtest/gtest.h"
+#include "Maps.h"
 
 #include "MiningOptimization/ObservationDataFiles.h"
 #include "MiningOptimization/GatherPositionObservations.h"
 #include "MiningOptimization/ResourceObservations.h"
 
+namespace
+{
+    void shrink(
+            const Maps::MapMetadata &map,
+            WorkerMiningOptimization::ObservationDataFiles::GameParameters &parameters)
+    {
+        parameters.mapHash = map.openbwHash;
+
+        Log::initialize();
+        Log::SetOutputToConsole(true);
+
+        Log::Get() << "Starting " << map.shortfilename();
+
+        WorkerMiningOptimization::ObservationDataFiles::overrideGameParameters(parameters);
+
+        std::map<TilePosition, std::unordered_map<PositionAndVelocity, WorkerMiningOptimization::GatherPositionObservations>>
+                resourceToOptimalGatherPositions;
+        WorkerMiningOptimization::ObservationDataFiles::readGatherPositionObservations(true, resourceToOptimalGatherPositions);
+        WorkerMiningOptimization::ObservationDataFiles::reduceGatherData(resourceToOptimalGatherPositions);
+        WorkerMiningOptimization::ObservationDataFiles::writeGatherPositionObservations(true, resourceToOptimalGatherPositions, true);
+
+        std::map<TilePosition, std::unordered_map<PositionAndVelocity, WorkerMiningOptimization::ReturnPositionObservations>>
+                resourceToOptimalReturnPositions;
+        WorkerMiningOptimization::ObservationDataFiles::readReturnPositionObservations(true, resourceToOptimalReturnPositions);
+        WorkerMiningOptimization::ObservationDataFiles::reduceReturnData(resourceToOptimalReturnPositions);
+        WorkerMiningOptimization::ObservationDataFiles::writeReturnPositionObservations(true, resourceToOptimalReturnPositions, true);
+
+        std::map<TilePosition, std::unordered_set<PositionAndVelocity>> resourceTo10DistancePositions;
+        WorkerMiningOptimization::ObservationDataFiles::read10DistanceObservations(resourceTo10DistancePositions);
+        WorkerMiningOptimization::ObservationDataFiles::write10DistanceObservations(resourceTo10DistancePositions, true);
+
+        std::map<TilePosition, WorkerMiningOptimization::ResourceObservations> resourceToResourceObservations;
+        WorkerMiningOptimization::ObservationDataFiles::readResourceObservations(true, resourceToResourceObservations);
+        WorkerMiningOptimization::ObservationDataFiles::writeResourceObservations(true, resourceToResourceObservations, true);
+
+        Log::Get() << "Finished " << map.shortfilename();
+    }
+}
+
 TEST(MiningDataShrinker, Vermeer)
 {
-    Log::initialize();
-    Log::SetOutputToConsole(true);
+    auto parameters = WorkerMiningOptimization::ObservationDataFiles::GameParameters{"", 3, 12, 5, 15, 5};
 
-    WorkerMiningOptimization::ObservationDataFiles::overrideGameParameters(
-            WorkerMiningOptimization::ObservationDataFiles::GameParameters{"0a306408d42d64cdef654b36286903b411246714", 3, 12, 5, 15, 5}
-    );
+    shrink(*Maps::GetOne("Vermeer"), parameters);
+}
 
-    std::map<TilePosition, std::unordered_map<PositionAndVelocity, WorkerMiningOptimization::GatherPositionObservations>>
-        resourceToOptimalGatherPositions;
-    WorkerMiningOptimization::ObservationDataFiles::readGatherPositionObservations(true, resourceToOptimalGatherPositions);
-    WorkerMiningOptimization::ObservationDataFiles::reduceGatherData(resourceToOptimalGatherPositions);
-    WorkerMiningOptimization::ObservationDataFiles::writeGatherPositionObservations(true, resourceToOptimalGatherPositions, true);
+TEST(MiningDataShrinker, SSCAIT)
+{
+    auto parameters = WorkerMiningOptimization::ObservationDataFiles::GameParameters{"", 3, 12, 5, 15, 5};
 
-    std::map<TilePosition, std::unordered_map<PositionAndVelocity, WorkerMiningOptimization::ReturnPositionObservations>>
-            resourceToOptimalReturnPositions;
-    WorkerMiningOptimization::ObservationDataFiles::readReturnPositionObservations(true, resourceToOptimalReturnPositions);
-    WorkerMiningOptimization::ObservationDataFiles::reduceReturnData(resourceToOptimalReturnPositions);
-    WorkerMiningOptimization::ObservationDataFiles::writeReturnPositionObservations(true, resourceToOptimalReturnPositions, true);
-
-    std::map<TilePosition, std::unordered_set<PositionAndVelocity>> resourceTo10DistancePositions;
-    WorkerMiningOptimization::ObservationDataFiles::read10DistanceObservations(resourceTo10DistancePositions);
-    WorkerMiningOptimization::ObservationDataFiles::write10DistanceObservations(resourceTo10DistancePositions, true);
-
-    std::map<TilePosition, WorkerMiningOptimization::ResourceObservations> resourceToResourceObservations;
-    WorkerMiningOptimization::ObservationDataFiles::readResourceObservations(true, resourceToResourceObservations);
-    WorkerMiningOptimization::ObservationDataFiles::writeResourceObservations(true, resourceToResourceObservations, true);
+    for (const auto &map : Maps::Get("sscai"))
+    {
+        shrink(map, parameters);
+    }
 }
