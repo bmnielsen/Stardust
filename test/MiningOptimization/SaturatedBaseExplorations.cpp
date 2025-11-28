f8efb847c04c96798f270e8b132e945d03de80e0
Bruce Nielsen <bmnielsen@users.noreply.github.com>
2025-11-28 23:42:33 +0100

    Update saturated test to double-check the behaviour when lock happens on approach

--- test/MiningOptimization/SaturatedBaseExplorations.cpp
+++ test/MiningOptimization/SaturatedBaseExplorations.cpp
@@ -18,15 +18,19 @@ TEST(SaturatedBase, SinglePatch)
     test.frameLimit = 1000;
     test.expectWin = false;
 
-    std::vector<BWAPI::Unit> otherPatches;
     BWAPI::Unit targetPatch;
+    BWAPI::Unit otherPatch;
 
-    std::vector<std::pair<BWAPI::Unit, BWAPI::Unit>> otherWorkersAndPatch;
-    std::vector<BWAPI::Unit> testWorkers;
+    int stopOtherWorkersMiningFrame = 190;
+    int stopMiningWorkerFrame = -1;
+    int resendApproachingWorkerFrame = 178;
 
-    int stopFrame = 283; // Frame when all other patch workers are ordered to stop
-    int resendOnApproachFrame = 261; // Frame when an approaching worker is ordered to gather the target patch again
-    int resendWhileMiningFrame = 220; // Frame when an approaching worker is ordered to gather the target patch again
+//    std::vector<std::pair<BWAPI::Unit, BWAPI::Unit>> otherWorkersAndPatch;
+//    std::vector<BWAPI::Unit> testWorkers;
+//
+////    int stopFrame = 283; // Frame when all other patch workers are ordered to stop
+//    int resendOnApproachFrame = 261; // Frame when an approaching worker is ordered to gather the target patch again
+//    int resendWhileMiningFrame = 220; // Frame when an approaching worker is ordered to gather the target patch again
 
     test.onFrameMine = [&]()
     {
@@ -39,162 +43,170 @@ TEST(SaturatedBase, SinglePatch)
                 {
                     targetPatch = patch;
                 }
+                else if (patch->getTilePosition() == BWAPI::TilePosition(3, 12))
+                {
+                    otherPatch = patch;
+                }
                 else
                 {
-                    otherPatches.push_back(patch);
+                    BWAPI::Broodwar->killUnit(patch);
                 }
             }
 
+            for (auto &worker : BWAPI::Broodwar->self()->getUnits())
+            {
+                if (worker->getType().isWorker()) BWAPI::Broodwar->killUnit(worker);
+            }
+        }
+
+        if (BWAPI::Broodwar->getFrameCount() == 15)
+        {
             BWAPI::Broodwar->createUnit(BWAPI::Broodwar->self(),
                                         BWAPI::UnitTypes::Protoss_Probe,
-                                        BWAPI::Position(240 + 24 * 4, 296));
+                                        targetPatch->getPosition() + BWAPI::Position(64, 0));
             BWAPI::Broodwar->createUnit(BWAPI::Broodwar->self(),
                                         BWAPI::UnitTypes::Protoss_Probe,
-                                        BWAPI::Position(240 + 24 * 5, 296));
+                                        targetPatch->getPosition() + BWAPI::Position(96, 0));
             BWAPI::Broodwar->createUnit(BWAPI::Broodwar->self(),
                                         BWAPI::UnitTypes::Protoss_Probe,
-                                        BWAPI::Position(240 + 24 * 6, 296));
+                                        otherPatch->getPosition() + BWAPI::Position(64, 0));
             BWAPI::Broodwar->createUnit(BWAPI::Broodwar->self(),
                                         BWAPI::UnitTypes::Protoss_Probe,
-                                        BWAPI::Position(240 + 24 * 7, 296));
-            BWAPI::Broodwar->createUnit(BWAPI::Broodwar->self(),
-                                        BWAPI::UnitTypes::Protoss_Probe,
-                                        BWAPI::Position(240 + 24 * 0, 296 + 32));
-            BWAPI::Broodwar->createUnit(BWAPI::Broodwar->self(),
-                                        BWAPI::UnitTypes::Protoss_Probe,
-                                        BWAPI::Position(240 + 24 * 1, 296 + 32));
-            BWAPI::Broodwar->createUnit(BWAPI::Broodwar->self(),
-                                        BWAPI::UnitTypes::Protoss_Probe,
-                                        BWAPI::Position(240 + 24 * 2, 296 + 32));
-            BWAPI::Broodwar->createUnit(BWAPI::Broodwar->self(),
-                                        BWAPI::UnitTypes::Protoss_Probe,
-                                        BWAPI::Position(240 + 24 * 3, 296 + 32));
-            BWAPI::Broodwar->createUnit(BWAPI::Broodwar->self(),
-                                        BWAPI::UnitTypes::Protoss_Probe,
-                                        BWAPI::Position(240 + 24 * 4, 296 + 32));
-            BWAPI::Broodwar->createUnit(BWAPI::Broodwar->self(),
-                                        BWAPI::UnitTypes::Protoss_Probe,
-                                        BWAPI::Position(240 + 24 * 5, 296 + 32));
-            BWAPI::Broodwar->createUnit(BWAPI::Broodwar->self(),
-                                        BWAPI::UnitTypes::Protoss_Probe,
-                                        BWAPI::Position(240 + 24 * 6, 296 + 32));
-            BWAPI::Broodwar->createUnit(BWAPI::Broodwar->self(),
-                                        BWAPI::UnitTypes::Protoss_Probe,
-                                        BWAPI::Position(240 + 24 * 7, 296 + 32));
-            BWAPI::Broodwar->createUnit(BWAPI::Broodwar->self(),
-                                        BWAPI::UnitTypes::Protoss_Probe,
-                                        BWAPI::Position(240 + 24 * 0, 296 + 64));
-            BWAPI::Broodwar->createUnit(BWAPI::Broodwar->self(),
-                                        BWAPI::UnitTypes::Protoss_Probe,
-                                        BWAPI::Position(240 + 24 * 1, 296 + 64));
-            BWAPI::Broodwar->createUnit(BWAPI::Broodwar->self(),
-                                        BWAPI::UnitTypes::Protoss_Probe,
-                                        BWAPI::Position(240 + 24 * 2, 296 + 64));
-            BWAPI::Broodwar->createUnit(BWAPI::Broodwar->self(),
-                                        BWAPI::UnitTypes::Protoss_Probe,
-                                        BWAPI::Position(240 + 24 * 3, 296 + 64));
-            BWAPI::Broodwar->createUnit(BWAPI::Broodwar->self(),
-                                        BWAPI::UnitTypes::Protoss_Probe,
-                                        BWAPI::Position(240 + 24 * 4, 296 + 64));
-            BWAPI::Broodwar->createUnit(BWAPI::Broodwar->self(),
-                                        BWAPI::UnitTypes::Protoss_Probe,
-                                        BWAPI::Position(240 + 24 * 5, 296 + 64));
-            BWAPI::Broodwar->createUnit(BWAPI::Broodwar->self(),
-                                        BWAPI::UnitTypes::Protoss_Probe,
-                                        BWAPI::Position(240 + 24 * 6, 296 + 64));
-            BWAPI::Broodwar->createUnit(BWAPI::Broodwar->self(),
-                                        BWAPI::UnitTypes::Protoss_Probe,
-                                        BWAPI::Position(240 + 24 * 7, 296 + 64));
+                                        otherPatch->getPosition() + BWAPI::Position(96, 0));
         }
 
         if (BWAPI::Broodwar->getFrameCount() == 20)
         {
-            auto patchIt = otherPatches.begin();
             for (auto &worker : BWAPI::Broodwar->self()->getUnits())
             {
-                if (!worker->getType().isWorker()) continue;
-                worker->gather(*patchIt);
-                otherWorkersAndPatch.emplace_back(worker, *patchIt);
-                patchIt++;
-                if (patchIt == otherPatches.end()) patchIt = otherPatches.begin();
-            }
-        }
-
-        if (BWAPI::Broodwar->getFrameCount() > 20 && BWAPI::Broodwar->getFrameCount() < stopFrame)
-        {
-            for (auto &[worker, patch] : otherWorkersAndPatch)
-            {
-                if (!worker->isCarryingMinerals() && worker->getOrderTarget() != patch)
+                if (worker->getDistance(otherPatch) < 64)
                 {
-                    worker->gather(patch);
+                    worker->gather(otherPatch);
+                }
+                else
+                {
+                    worker->gather(targetPatch);
                 }
             }
         }
 
-        if (BWAPI::Broodwar->getFrameCount() == 50)
-        {
-            BWAPI::Broodwar->createUnit(BWAPI::Broodwar->self(),
-                                        BWAPI::UnitTypes::Protoss_Probe,
-                                        BWAPI::Position(240 + 48, 296 + 32));
-            BWAPI::Broodwar->createUnit(BWAPI::Broodwar->self(),
-                                        BWAPI::UnitTypes::Protoss_Probe,
-                                        BWAPI::Position(240 + 72, 296 + 32));
-        }
-
-        if (BWAPI::Broodwar->getFrameCount() == 60)
+        if (BWAPI::Broodwar->getFrameCount() > 20)
         {
             for (auto &worker : BWAPI::Broodwar->self()->getUnits())
             {
-                if (!worker->getType().isWorker()) continue;
-                if (worker->getLastCommand().getType() == BWAPI::UnitCommandTypes::Gather) continue;
-                testWorkers.emplace_back(worker);
-                worker->gather(targetPatch);
+                if (worker->getDistance(otherPatch) < 64)
+                {
+                    if (BWAPI::Broodwar->getFrameCount() >= stopOtherWorkersMiningFrame)
+                    {
+                        worker->stop();
+                    }
+                    else
+                    {
+                        if (worker->getOrder() == BWAPI::Orders::MiningMinerals && worker->getOrderTimer() == 40)
+                        {
+                            worker->stop();
+                        }
+                        if (worker->getOrder() == BWAPI::Orders::PlayerGuard)
+                        {
+                            worker->gather(otherPatch);
+                        }
+                    }
+                }
+                else if (BWAPI::Broodwar->getFrameCount() == stopMiningWorkerFrame && worker->getOrder() == BWAPI::Orders::MiningMinerals)
+                {
+                    worker->stop();
+                }
+                else if (BWAPI::Broodwar->getFrameCount() == resendApproachingWorkerFrame && worker->getOrder() == BWAPI::Orders::MoveToMinerals)
+                {
+                    worker->gather(targetPatch);
+                }
+                else if (BWAPI::Broodwar->getFrameCount() == (resendApproachingWorkerFrame - 15) && worker->getOrder() == BWAPI::Orders::MiningMinerals)
+                {
+                    worker->gather(targetPatch);
+                }
             }
         }
 
-        if (BWAPI::Broodwar->getFrameCount() == resendOnApproachFrame)
-        {
-            for (auto &worker : testWorkers)
-            {
-                if (worker->getOrder() != BWAPI::Orders::MoveToMinerals) continue;
-                worker->gather(targetPatch);
-            }
-        }
-
-        if (BWAPI::Broodwar->getFrameCount() == resendWhileMiningFrame)
-        {
-            for (auto &worker : testWorkers)
-            {
-                if (worker->getOrder() != BWAPI::Orders::MiningMinerals) continue;
-                worker->gather(targetPatch);
-            }
-        }
-
-        if (BWAPI::Broodwar->getFrameCount() > 60)
-        {
-            for (auto &worker : testWorkers)
-            {
-//                if (BWAPI::Broodwar->getFrameCount() == 277)
+//        if (BWAPI::Broodwar->getFrameCount() > 20)
+//        {
+//            for (auto &[worker, patch] : otherWorkersAndPatch)
+//            {
+//                if (worker->getOrder() == BWAPI::Orders::MiningMinerals && worker->getOrderTimer() == 40)
 //                {
-//                    std::cout << worker->getOrder() << " at dist " << worker->getDistance(worker->getOrderTarget()) << " timer " << worker->getOrderProcessTimer()
+//                    worker->stop();
 //                }
-
-                if (worker->getOrder() != BWAPI::Orders::MoveToMinerals) continue;
-                if (worker->getOrderProcessTimer() != 8) continue;
-                if (worker->getDistance(worker->getOrderTarget()) > 10) continue;
-                if (worker->getDistance(worker->getOrderTarget()) == 0) continue;
-
-                std::cout << BWAPI::Broodwar->getFrameCount() << ": Worker " << worker->getID() << " approaching at order timer 0" << std::endl;
-            }
-        }
-
-        if (BWAPI::Broodwar->getFrameCount() == stopFrame)
-        {
-            for (auto &[worker, _] : otherWorkersAndPatch)
-            {
-                worker->stop();
-            }
-        }
+//                if ((worker->getOrder() == BWAPI::Orders::MoveToMinerals && worker->getOrderTarget() != patch)
+//                    || worker->getOrder() == BWAPI::Orders::PlayerGuard)
+//                {
+//                    worker->gather(patch);
+//                }
+//            }
+//        }
+//
+//        if (BWAPI::Broodwar->getFrameCount() == 50)
+//        {
+//            BWAPI::Broodwar->createUnit(BWAPI::Broodwar->self(),
+//                                        BWAPI::UnitTypes::Protoss_Probe,
+//                                        BWAPI::Position(240 + 48, 296 + 32));
+//            BWAPI::Broodwar->createUnit(BWAPI::Broodwar->self(),
+//                                        BWAPI::UnitTypes::Protoss_Probe,
+//                                        BWAPI::Position(240 + 72, 296 + 32));
+//        }
+//
+//        if (BWAPI::Broodwar->getFrameCount() == 60)
+//        {
+//            for (auto &worker : BWAPI::Broodwar->self()->getUnits())
+//            {
+//                if (!worker->getType().isWorker()) continue;
+//                if (worker->getLastCommand().getType() != BWAPI::UnitCommandTypes::None) continue;
+//                testWorkers.emplace_back(worker);
+//                worker->gather(targetPatch);
+//            }
+//        }
+//
+//        if (BWAPI::Broodwar->getFrameCount() == resendOnApproachFrame)
+//        {
+//            for (auto &worker : testWorkers)
+//            {
+//                if (worker->getOrder() != BWAPI::Orders::MoveToMinerals) continue;
+//                worker->gather(targetPatch);
+//            }
+//        }
+//
+//        if (BWAPI::Broodwar->getFrameCount() == resendWhileMiningFrame)
+//        {
+//            for (auto &worker : testWorkers)
+//            {
+//                if (worker->getOrder() != BWAPI::Orders::MiningMinerals) continue;
+//                worker->gather(targetPatch);
+//            }
+//        }
+//
+//        if (BWAPI::Broodwar->getFrameCount() > 60)
+//        {
+//            for (auto &worker : testWorkers)
+//            {
+////                if (BWAPI::Broodwar->getFrameCount() == 277)
+////                {
+////                    std::cout << worker->getOrder() << " at dist " << worker->getDistance(worker->getOrderTarget()) << " timer " << worker->getOrderProcessTimer()
+////                }
+//
+//                if (worker->getOrder() != BWAPI::Orders::MoveToMinerals) continue;
+//                if (worker->getOrderProcessTimer() != 8) continue;
+//                if (worker->getDistance(worker->getOrderTarget()) > 10) continue;
+//                if (worker->getDistance(worker->getOrderTarget()) == 0) continue;
+//
+//                std::cout << BWAPI::Broodwar->getFrameCount() << ": Worker " << worker->getID() << " approaching at order timer 0" << std::endl;
+//            }
+//        }
+//
+//        if (BWAPI::Broodwar->getFrameCount() == stopFrame)
+//        {
+//            for (auto &[worker, _] : otherWorkersAndPatch)
+//            {
+//                worker->stop();
+//            }
+//        }
     };
     test.run();
 }
