1f45572747accd4608fc4ab7f41944c6356b85f3
Bruce Nielsen <bmnielsen@users.noreply.github.com>
2025-12-16 21:57:19 +0100

    Disable per-patch output

--- test/MiningOptimization/FullSaturationTraining.cpp
+++ test/MiningOptimization/FullSaturationTraining.cpp
@@ -1024,7 +1024,7 @@ TEST(FullSaturationTraining, VermeerSingleMeasure)
     BWTest test;
     test.map = Maps::GetOne("VermeerSE_2.1");
     test.randomSeed = 42;
-    auto sgl = runEfficiencyTest(test, 1, 0, false, true, true);
+    auto sgl = runEfficiencyTest(test, 1, 0, false, true, false);
     std::cout << std::fixed << std::showpoint << std::setprecision(4)
               << "Overall efficiency: " << std::endl
               << "Single: " << sgl << std::endl;
@@ -1035,7 +1035,7 @@ TEST(FullSaturationTraining, VermeerSingleMeasureOneBase)
     BWTest test;
     test.map = Maps::GetOne("VermeerSE_2.1");
     test.randomSeed = 42;
-    auto sgl = runEfficiencyTest(test, 1, 0, false, true, true, true);
+    auto sgl = runEfficiencyTest(test, 1, 0, false, true, false, true);
     std::cout << std::fixed << std::showpoint << std::setprecision(4)
               << "Overall efficiency: " << std::endl
               << "Single: " << sgl << std::endl;
