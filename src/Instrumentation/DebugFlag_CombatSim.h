e84b6ed19032fd30d29e1bef7350deba3a96b3b8
Bruce Nielsen <bmnielsen@users.noreply.github.com>
2026-06-30 21:29:07 +0200

    Attempt to enable verbose sim logging in docker

--- src/Instrumentation/DebugFlag_CombatSim.h
+++ src/Instrumentation/DebugFlag_CombatSim.h
@@ -3,9 +3,9 @@
 #if INSTRUMENTATION_ENABLED
 #define DEBUG_COMBATSIM false       // Draws each sim result and writes log messages for interesting transitions
 #define DEBUG_COMBATSIM_LOG false   // Writes log messages for each sim result
+#define DEBUG_COMBATSIM_CVIS true        // Writes combat sim data to cherryvis
 #endif
 
 #if INSTRUMENTATION_ENABLED_VERBOSE
-#define DEBUG_COMBATSIM_CVIS true        // Writes combat sim data to cherryvis
 #define DEBUG_COMBATSIM_EACHFRAME false   // Writes values after each frame of the sim
 #endif
